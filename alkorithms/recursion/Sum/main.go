592fb7f1071c987ff6eb36a442dc0e1dcba83825
Alexey Polukhin <alexpolukhin@yandex.ru>
2025-12-20 11:57:28 +0300

    Some recursion added

--- alkorithms/recursion/Sum/main.go
+++ alkorithms/recursion/Sum/main.go
@@ -11,7 +11,18 @@ func SumOfSlice(arr []int) int {
 	return arr[0] + SumOfSlice(arr[1:])
 }
 
+// SumByIndex с использованием индексов
+// чтобы не создавались заново слайсы в ходе рекурсии
+func SumByIndex(arr []int, index int) int {
+	if len(arr) == index {
+		return 0
+	}
+	return arr[index] + SumByIndex(arr, index+1)
+}
+
 func main() {
 	var a = []int{1, 2, 3}
 	fmt.Println(SumOfSlice(a))
+
+	fmt.Println(SumByIndex(a, 0))
 }
