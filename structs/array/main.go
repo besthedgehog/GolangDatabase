4fc597d1c1f91f529ebc893a76d02ee7520e34bc
Alexey Polukhin <alexpolukhin@yandex.ru>
2026-01-17 12:54:05 +0300

    Unstable

--- structs/array/main.go
+++ structs/array/main.go
@@ -10,16 +10,12 @@ type DynamicArray[T any] struct {
 	arr      []T
 }
 
-// func NewDynamicArray[T any](capacity int) *DynamicArray[T] {
-// 	if capacity <= 0 {
-// 		panic("Array capacity cannot be <= 0")
-// 	}
-
-// 	return &DynamicArray[T]{
-// 		capacity: capacity,
-// 		arr:      make([]T, capacity),
-// 	}
-// }
+// Фабрика
+func NewDynamicArray[T any](capacity int) *DynamicArray[T] {
+	return &DynamicArray[T]{
+		arr: make([]T, 0, capacity),
+	}
+}
 
 // Проверка на правильность обращения по индексу
 // чтобы не было паники
@@ -33,9 +29,8 @@ func (da *DynamicArray[T]) checkRangeFromIndex(index int) error {
 
 // Выделение увеличение размера памяти, выделяемого под массив
 func (da *DynamicArray[T]) newCapacity() {
-	da.capacity = da.capacity * 2
-
-	newArr := make([]T, da.capacity)
+	da.capacity *= 2
+	newArr := make([]T, da.length, da.capacity)
 	copy(newArr, da.arr)
 	da.arr = newArr
 }
@@ -45,16 +40,6 @@ func (da *DynamicArray[T]) IsEmpty() bool {
 	return da.length == 0
 }
 
-// // Добавление нового элемента в массив
-// func (da *DynamicArray[T]) Add(element T) {
-// 	if da.length == da.capacity {
-// 		da.newCapacity()
-// 	}
-
-// 	da.arr[da.length] = element
-// 	da.length++
-// }
-
 // Удаление элемента. Смещаем все элементы на один индекс влево
 func (da *DynamicArray[T]) Remove(index int) error {
 	err := da.checkRangeFromIndex(index)
@@ -63,10 +48,15 @@ func (da *DynamicArray[T]) Remove(index int) error {
 	}
 
 	copy(da.arr[index:], da.arr[index+1:])
+	// Пояснение
+	// dst:   [2 3 4]
+	// src:   [3 4]
+	// res: [1 3 4 4]
 
-	// Делаем последний элемент нулевым, чтобы не было утечек памяти
-	da.arr[da.length-1] = *new(T) // зачем
-	da.length--
+	// Теперь узбавимся от последнего элемента (дубликата)
+	var zero T
+	da.arr[len(da.arr)-1] = zero
+	da.arr = da.arr[:len(da.arr)-1]
 	return nil
 }
 
@@ -93,12 +83,6 @@ type Dog struct {
 	age  uint8
 }
 
-func NewDynamicArray[T any](capacity int) *DynamicArray[T] {
-	return &DynamicArray[T]{
-		arr: make([]T, 0, capacity),
-	}
-}
-
 func (da *DynamicArray[T]) Add(element T) {
 	if len(da.arr) == cap(da.arr) {
 		newArr := make([]T, len(da.arr), cap(da.arr)*2)
@@ -106,7 +90,9 @@ func (da *DynamicArray[T]) Add(element T) {
 		da.arr = newArr
 	}
 
+	// Увеличим длину массива на 1, чтобы не было паники при добавлении
 	da.arr = da.arr[:len(da.arr)+1]
+	// добавим элемент
 	da.arr[len(da.arr)-1] = element
 }
 
@@ -117,4 +103,5 @@ func main() {
 	// dynamicArray.Add(Dog{"Alex", 3})
 	// dynamicArray.Add(Dog{"Misha", 1})
 	fmt.Println(dynamicArray)
+
 }
