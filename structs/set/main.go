d9a4bba2cf800246756eccd06a9a1492d520ae44
Alexey Polukhin <alexpolukhin@yandex.ru>
2026-09-06 13:23:04 +0300

    Set

--- structs/set/main.go
+++ structs/set/main.go
@@ -128,3 +128,29 @@ func (s *Set[T]) PrintSet() {
 	})
 	fmt.Println("]")
 }
+
+func main() {
+	setA := NewSet(1, 2, 3, 4, 5, 6, 3, 3, 3, 2, 1, 4, 7, 8, 5)
+	fmt.Println("----- setA ------")
+	setA.PrintSet()
+	setB := NewSet(10, 22, 1, 4, 21, 4, 5, 21, 11, 10)
+	fmt.Println("----- setB ------")
+	setB.PrintSet()
+	fmt.Println("----- Contains ------")
+	value := 10
+	fmt.Printf("SetA contains %v == %v\n",
+		value, setA.Contains(value))
+	fmt.Printf("SetB contains %v == %v\n", value, setB.Contains(value))
+
+	fmt.Println("----- A - B ------")
+	setA.Difference(setB).PrintSet()
+
+	fmt.Println("----- B - A ------")
+	setB.Difference(setA).PrintSet()
+
+	fmt.Println("----- Union ------")
+	setA.Union(setB).PrintSet()
+
+	fmt.Println("----- Intersect ------")
+	setA.Intersect(setB).PrintSet()
+}
